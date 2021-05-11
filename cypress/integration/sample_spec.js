e528f9f49e139be79cf6f37b3b94207f3878c84f
Case Prince <caseprince@gmail.com>
2021-05-11 17:05:38 -0400

    Install @cypress/code-coverage, instrument-cra

--- cypress/integration/sample_spec.js
+++ cypress/integration/sample_spec.js
@@ -1,5 +1,8 @@
 describe("My First Test", () => {
   it("Does not do much!", () => {
-    cy.visit("http://localhost:3000/");
+    cy.visit("http://localhost:3000/")
+      .wait(1000)
+      .get(".App")
+      .should("have.length", 1);
   });
 });
